0abea238e7428de01f0ca3cee5fd4f207a5f9b56
David Hary <791370+ATECoder@users.noreply.github.com>
2025-11-19 21:19:17 -0800

    - Access subsystem base: update implementation.

--- changelog.md
+++ changelog.md
@@ -2,9 +2,12 @@
 Notable changes to the `cc.isr.vi` solution are documented in this file using the 
 [Keep a Changelog] style. The dates specified are in coordinated universal time (UTC).
 
-[8.1.9454]: https://www.github.com/atecoder/dn.vi.ivi
+[8.1.9455]: https://www.github.com/atecoder/dn.vi.ivi
 
 Current IVI Visa Compatibility: 8.0.1.9391
+
+## [8.1.9455] - 2025-11-20
+- Access subsystem base: update implementation.
 
 ## [8.1.9454] - 2025-11-19
 Device Tsp
--- src/device/device/Subsystems/Misc/AccessSubsystemBase.cs
+++ src/device/device/Subsystems/Misc/AccessSubsystemBase.cs
@@ -31,17 +31,12 @@ public abstract partial class AccessSubsystemBase( StatusSubsystemBase statusSub
 
     #endregion
 
-    #region " api access management "
-
-    private TimeSpan _certifyTimeout;
+    #region " certification "
 
     /// <summary> Gets or sets the timeout time span allowed for certifying the device. </summary>
     /// <value> The certify timeout. </value>
-    public TimeSpan CertifyTimeout
-    {
-        get => this._certifyTimeout;
-        set => this.SetProperty( ref this._certifyTimeout, value );
-    }
+    [ObservableProperty]
+    public partial TimeSpan CertifyTimeout { get; set; }
 
     /// <summary> Gets or sets the cached certification sentinel. </summary>
     /// <value>
@@ -50,99 +45,58 @@ public abstract partial class AccessSubsystemBase( StatusSubsystemBase statusSub
     [ObservableProperty]
     public partial bool? Certified { get; protected set; }
 
-    /// <summary>   Queries if the instrument is certified for API access. </summary>
-    /// <remarks>   2025-05-26. </remarks>
-    /// <param name="details">  [out] The details. </param>
-    /// <returns>   True if certified, false if not. </returns>
-    public abstract bool IsCertified( out string details );
-
-    /// <summary>   Queries if the instrument is registered and certified for API access. </summary>
-    /// <remarks>   2025-05-26. </remarks>
-    /// <param name="details">  [out] The details. </param>
-    /// <returns>   True if registered and certified, false if not. </returns>
-    public abstract bool IsRegisteredAndCertified( out string details );
-
-    /// <summary>   Queries if the instrument is registered and certified for API access. </summary>
-    /// <remarks>   2025-06-03. </remarks>
-    /// <param name="serialNumber"> The serial number. </param>
-    /// <param name="details">      [out] The details. </param>
-    /// <returns>   True if registered and certified, false if not. </returns>
-    public bool IsRegisteredAndCertified( string serialNumber, out string details )
+    /// <summary>   Rerturns the certification state. </summary>
+    /// <remarks>   2025-11-19. </remarks>
+    /// <returns>   A string. </returns>
+    public string CertificationState()
     {
-        // If the serial number is not specified, use the connected instrument's serial number.
-        if ( string.IsNullOrEmpty( serialNumber ) )
-            serialNumber = this.StatusSubsystem.VersionInfoBase.SerialNumber;
-        if ( string.IsNullOrEmpty( serialNumber ) )
-        {
-            details = "Serial number is not specified and the connected instrument does not have a serial number.";
-            return false;
-        }
-        else if ( !serialNumber.Equals( this.StatusSubsystem.VersionInfoBase.SerialNumber, StringComparison.Ordinal ) )
-        {
-            details = $"Serial number '{serialNumber}' does not match the connected instrument's serial number '{this.StatusSubsystem.VersionInfoBase.SerialNumber}'.";
-            return false;
-        }
-        return this.IsRegistered( serialNumber, out details ) && this.IsCertified( out details );
+        return this.Certified.HasValue
+            ? this.Certified.Value
+                ? "Certified for API access."
+                : "Not certified for API access."
+            : "Certification state is unknown.";
     }
 
-    /// <summary>   Attempts to certifies the connected instrument for Api access if it is registered. </summary>
-    /// <remarks>   2024-08-24. </remarks>
-    /// <param name="details">          [out] The details. </param>
-    /// <returns>   <c>true</c> if the instrument is certified; otherwise, false. </returns>
-    public abstract bool TryCertifyIfRegistered( out string details );
+    /// <summary>   Extended certification state. </summary>
+    /// <remarks>   2025-11-19. </remarks>
+    /// <returns>   A string. </returns>
+    public string ExtendedCertificationState()
+    {
+        return this.Certified.HasValue
+                ? this.Certified.Value
+                   ? string.Empty
+                   : $"The {this.Session.ResourceNameCaption} with serial number {this.StatusSubsystem.VersionInfoBase.SerialNumber} is not certified for API access."
+                : "Unable to determine the API certification because the certification script is not loaded.";
+    }
 
-    /// <summary>
-    /// Attempts to register and certify a connected instrument.
-    /// </summary>
-    /// <remarks>   2025-05-26. </remarks>
-    /// <param name="details">          [out] The details. </param>
-    /// <returns>   True if it succeeds; otherwise, false. </returns>
-    public abstract bool TryRegisterAndCertify( out string details );
+    /// <summary>   Checks the instrument certification for API access. </summary>
+    /// <remarks>   2025-06-03. </remarks>
+    /// <exception cref="InvalidOperationException">    Thrown when the requested operation is
+    ///                                                 invalid. </exception>
+    public abstract void CheckCertification();
 
-    /// <summary>
-    /// Tries to decertify the connected instrument. This does not remove the instrument from the
-    /// registration dictionary.
-    /// </summary>
-    /// <remarks>   2024-12-09. </remarks>
+    /// <summary>   Attempts to check if the instrument is certified for API access. </summary>
+    /// <remarks>   2025-06-03. </remarks>
     /// <param name="details">  [out] The details. </param>
-    /// <returns>   True if it succeeds; otherwise, false. </returns>
-    public abstract bool TryDecertify( out string details );
-
-    /// <summary>   Attempts to register an instrument with the specified serial number. </summary>
-    /// <remarks>   2024-12-12. </remarks>
-    /// <param name="serialNumber">     The serial number. </param>
-    /// <param name="details">          [out] The details. </param>
-    /// <returns>   True if it succeeds; otherwise, false. </returns>
-    public abstract bool TryRegister( string serialNumber, out string details );
-
-    /// <summary>
-    /// Attempts to deregister an instrument with the specified serial number by removing this instrument from the
-    /// registration dictionary but not removing the API access code from the current instrument.
-    /// </summary>
-    /// <remarks>   2024-12-12. </remarks>
-    /// <param name="serialNumber"> The serial number. </param>
-    /// <param name="details">      [out] The details. </param>
-    /// <returns>   True if it succeeds; otherwise, false. </returns>
-    public abstract bool TryDeregister( string serialNumber, out string details );
-
-    /// <summary>   Queries an instrument with the specified serial number is registered. </summary>
-    /// <remarks>   2024-08-24. </remarks>
-    /// <param name="serialNumber"> The serial number. </param>
-    /// <param name="details">      [out] The details. </param>
-    /// <returns>   <c>true</c> if the instrument is certified; otherwise, false. </returns>
-    public abstract bool IsRegistered( string serialNumber, out string details );
-
-    /// <summary>   Validates the cached registration value. </summary>
-    /// <remarks>   2025-06-02. </remarks>
-    /// <param name="details">  [out] The details. </param>
-    /// <returns>   True if the registration is cached and is valid, otherwise, false. </returns>
-    public abstract bool ValidateRegistration( out string details );
-
-    /// <summary>   Queries if the connected instrument registration is cached in the connected instrument. </summary>
-    /// <remarks>   2025-06-02. </remarks>
-    /// <param name="details">      [out] The details. </param>
-    /// <returns>   <c>true</c> if the instrument registration code is cached in the instrument user string. </returns>
-    public abstract bool IsRegistrationCached( out string details );
+    /// <returns>   True if certified, false if not. </returns>
+    public bool TryCheckCertification( out string details )
+    {
+        try
+        {
+            this.CheckCertification();
+            details = this.Certified.HasValue
+                ? this.Certified.Value
+                   ? string.Empty
+                   : $"The {this.Session.ResourceNameCaption} with serial number {this.StatusSubsystem.VersionInfoBase.SerialNumber} is not certified for API access."
+                : "Unable to determine the API certification because the certification script is not loaded.";
+        }
+        catch ( Exception ex )
+        {
+            details = ex.Message;
+            throw;
+        }
+        return string.IsNullOrWhiteSpace( details );
+    }
 
     /// <summary>   Validates the cached certification value. </summary>
     /// <remarks>   2025-06-02. </remarks>
@@ -175,6 +129,215 @@ public abstract partial class AccessSubsystemBase( StatusSubsystemBase statusSub
     /// <remarks>   2025-06-02. </remarks>
     /// <param name="filePath"> Full pathname of the file. </param>
     public abstract void ImportCertificationCodes( string filePath );
+
+    /// <summary>   Decertifies this object. </summary>
+    /// <remarks>   2025-11-19. </remarks>
+    public abstract void Decertify();
+
+    /// <summary>
+    /// Tries to decertify the connected instrument. This does not remove the instrument from the
+    /// registration dictionary.
+    /// </summary>
+    /// <remarks>   2024-12-09. </remarks>
+    /// <param name="details">  [out] The details. </param>
+    /// <returns>   True if it succeeds; otherwise, false. </returns>
+    public bool TryDecertify( out string details )
+    {
+        details = string.Empty;
+        try
+        {
+            this.Decertify();
+        }
+        catch ( Exception ex )
+        {
+            details = ex.Message;
+        }
+        return string.IsNullOrWhiteSpace( details );
+    }
+
+    #endregion
+
+    #region " registration "
+
+    /// <summary>   Queries an instrument with the specified serial number is registered. </summary>
+    /// <remarks>   2024-08-24. </remarks>
+    /// <param name="serialNumber"> The serial number. </param>
+    /// <param name="details">      [out] The details. </param>
+    /// <returns>   <c>true</c> if the instrument is certified; otherwise, false. </returns>
+    public abstract bool IsRegistered( string serialNumber, out string details );
+
+    /// <summary>   Queries if the connected instrument is registered. </summary>
+    /// <remarks>   2024-12-13. </remarks>
+    /// <param name="details">  [out] The details. </param>
+    /// <returns>   <c>true</c> if the instrument is certified; otherwise, false. </returns>
+    public bool IsRegistered( out string details )
+    {
+        return this.IsRegistered( this.Session.QueryTrimEnd( cc.isr.VI.Syntax.Tsp.LocalNode.SerialNumberFormattedQueryCommand ), out details );
+    }
+
+    /// <summary>   Attempts to register an instrument with the specified serial number. </summary>
+    /// <remarks>   2024-12-12. </remarks>
+    /// <param name="serialNumber">     The serial number. </param>
+    /// <param name="details">          [out] The details. </param>
+    /// <returns>   True if it succeeds; otherwise, false. </returns>
+    public abstract bool TryRegister( string serialNumber, out string details );
+
+    /// <summary>   Attempts to register a connected instrument instrument. </summary>
+    /// <remarks>   2024-12-13. </remarks>
+    /// <param name="details">          [out] The details. </param>
+    /// <returns>   True if it succeeds; otherwise, false. </returns>
+    public bool TryRegister( out string details )
+    {
+        return this.TryRegister( this.Session.QueryTrimEnd( cc.isr.VI.Syntax.Tsp.LocalNode.SerialNumberFormattedQueryCommand ), out details );
+    }
+
+    /// <summary>   Deregisters this object. </summary>
+    /// <remarks>   2025-11-19. </remarks>
+    /// <param name="serialNumber"> The serial number. </param>
+    public abstract void Deregister( string serialNumber );
+
+    /// <summary>
+    /// Attempts to deregister an instrument with the specified serial number by removing this instrument from the
+    /// registration dictionary but not removing the API access code from the current instrument.
+    /// </summary>
+    /// <remarks>   2024-12-12. </remarks>
+    /// <param name="serialNumber"> The serial number. </param>
+    /// <param name="details">      [out] The details. </param>
+    /// <returns>   True if it succeeds; otherwise, false. </returns>
+    public bool TryDeregister( string serialNumber, out string details )
+    {
+        details = string.Empty;
+        try
+        {
+            this.Deregister( serialNumber );
+        }
+        catch ( Exception ex )
+        {
+            details = ex.Message;
+            throw;
+        }
+        return string.IsNullOrWhiteSpace( details );
+    }
+
+    /// <summary>
+    /// Attempts to deregister the active instrument by removing it instrument from the
+    /// registration dictionary but not removing the API access code from the current instrument.
+    /// </summary>
+    /// <remarks>   2024-12-13. </remarks>
+    /// <param name="details">  [out] The details. </param>
+    /// <returns>   True if it succeeds; otherwise, false. </returns>
+    public bool TryDeregister( out string details )
+    {
+        return this.TryDeregister( this.Session.QueryTrimEnd( cc.isr.VI.Syntax.Tsp.LocalNode.SerialNumberFormattedQueryCommand ), out details );
+    }
+
+    /// <summary>   Validates the cached registration value. </summary>
+    /// <remarks>   2025-06-02. </remarks>
+    /// <param name="details">  [out] The details. </param>
+    /// <returns>   True if the registration is cached and is valid, otherwise, false. </returns>
+    public abstract bool ValidateRegistration( out string details );
+
+    /// <summary>   Queries if the connected instrument registration is cached in the connected instrument. </summary>
+    /// <remarks>   2025-06-02. </remarks>
+    /// <param name="details">      [out] The details. </param>
+    /// <returns>   <c>true</c> if the instrument registration code is cached in the instrument user string. </returns>
+    public abstract bool IsRegistrationCached( out string details );
+
+    #endregion
+
+    #region " registration and certification "
+
+    /// <summary>   Queries if the instrument is registered and certified for API access. </summary>
+    /// <remarks>   2025-05-26. </remarks>
+    /// <param name="details">  [out] The details. </param>
+    /// <returns>   True if registered and certified, false if not. </returns>
+    public abstract bool IsRegisteredAndCertified( out string details );
+
+    /// <summary>   Queries if the instrument is registered and certified for API access. </summary>
+    /// <remarks>   2025-06-03. </remarks>
+    /// <param name="serialNumber"> The serial number. </param>
+    /// <param name="details">      [out] The details. </param>
+    /// <returns>   True if registered and certified, false if not. </returns>
+    public bool IsRegisteredAndCertified( string serialNumber, out string details )
+    {
+        // If the serial number is not specified, use the connected instrument's serial number.
+        if ( string.IsNullOrEmpty( serialNumber ) )
+            serialNumber = this.StatusSubsystem.VersionInfoBase.SerialNumber;
+        if ( string.IsNullOrEmpty( serialNumber ) )
+        {
+            details = "Serial number is not specified and the connected instrument does not have a serial number.";
+            return false;
+        }
+        else if ( !serialNumber.Equals( this.StatusSubsystem.VersionInfoBase.SerialNumber, StringComparison.Ordinal ) )
+        {
+            details = $"Serial number '{serialNumber}' does not match the connected instrument's serial number '{this.StatusSubsystem.VersionInfoBase.SerialNumber}'.";
+            return false;
+        }
+        return this.IsRegistered( serialNumber, out details )
+            && this.TryCheckCertification( out details );
+    }
+
+    /// <summary>   Certify if registered. </summary>
+    /// <remarks>   2025-11-19. </remarks>
+    /// <param name="serialNumber"> The serial number. </param>
+    /// <param name="functionName"> Name of the function. </param>
+    public abstract void CertifyIfRegistered( string serialNumber, string functionName );
+
+    /// <summary>   Certify if registered. </summary>
+    /// <remarks>   2025-11-19. </remarks>
+    public abstract void CertifyIfRegistered();
+
+    /// <summary>   Attempts to certifies the connected instrument for Api access if it is registered. </summary>
+    /// <remarks>   2024-08-24. </remarks>
+    /// <param name="details">          [out] The details. </param>
+    /// <returns>   <c>true</c> if the instrument is certified; otherwise, false. </returns>
+    public virtual bool TryCertifyIfRegistered( out string details )
+    {
+        try
+        {
+            this.CertifyIfRegistered();
+            details = this.Certified.GetValueOrDefault( false )
+                ? string.Empty
+                : "Instrument is not certified for API access.";
+        }
+        catch ( Exception ex )
+        {
+            details = ex.Message;
+        }
+        return string.IsNullOrWhiteSpace( details );
+    }
+
+    /// <summary>   Registers the controller instrument and certifies is for API access. </summary>
+    /// <remarks>   2024-12-11. </remarks>
+    public void RegisterAndCertify()
+    {
+        if ( this.TryRegister( this.Session.QueryTrimEnd( cc.isr.VI.Syntax.Tsp.LocalNode.SerialNumberFormattedQueryCommand ), out string details ) )
+        {
+            this.CertifyIfRegistered();
+        }
+        else
+            throw new InvalidOperationException( details );
+    }
+
+    /// <summary>
+    /// Attempts to register and certify a connected instrument.
+    /// </summary>
+    /// <remarks>   2025-05-26. </remarks>
+    /// <param name="details">          [out] The details. </param>
+    /// <returns>   True if it succeeds; otherwise, false. </returns>
+    public virtual bool TryRegisterAndCertify( out string details )
+    {
+        details = string.Empty;
+        try
+        {
+            this.RegisterAndCertify();
+        }
+        catch ( Exception ex )
+        {
+            details = ex.Message;
+        }
+        return string.IsNullOrWhiteSpace( details );
+    }
 
     #endregion
 
